186eda9aefdb5dcd20feed7d4549a8e7ccb506a6
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-01 00:27:24 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('myForm');
+  const submitButton = document.getElementById('submitButton');
+
+  // Add event listener to the form for the submit event
+  form.addEventListener('submit', function(event) {
+    // Prevent the default behavior of form submission
+    event.preventDefault();
+
+    // Perform additional actions or validations here
+
+    // Example: Log a message when the submit button is clicked
+    console.log('Submit button clicked!');
+  });
 });
+
+
+
+
+
+
